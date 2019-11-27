34a28a387e36ae5334b279f6a2b464a2659b939f
jhjhy0313 <jhjhy0313@naver.com>
2019-11-27 15:31:38 +0900

    1

--- first.R
+++ first.R
@@ -19,9 +19,72 @@ number2 <- number
 numberValue <- 1
 str_value <- "R Lunguage"
 booleanvalue <- TRUE
-
-numberValue <- "R script"
+numberValue <- 1
+numberValue
 print(numberValue)
 print(str_value)
 print(booleanvalue)
 
+cat("numeric number:",numberValue,"\n")
+cat("String :",str_value, "\n")
+cat("booleanvalue:",booleanvalue,"\n")
+
+numberValue <- scan()
+cat("Numeric number:",numberValue,"\n")
+
+number1 <- 10
+number2 <- 20
+resultAdd <- number1 <- number2
+resultSub <- number1 <- number2
+resultMul <- number1 <- number2
+resultDiv <- number1 / number2
+resultRem <- number2 %% number1
+resultSec<- number2 ^ 5
+
+print(resultAdd)
+print(resultSub)
+print(resultMul)
+print(resultDiv)
+print(resulRem)
+print(resultSec)
+
+number1 <- 0
+number1 <- number1+10
+number1
+number1 <- number1+10
+number1
+number1 <- number1+10
+number1
+
+number2 <- 100
+number1 <- number2+10
+number1
+number2
+
+print((number1+10)*number2/2)
+
+number301 <- number300+2
+number301
+
+number1 <- 10.5
+number2 <- 10
+print(number1>number2)
+print(number1>=number2)
+print(number1<number2)
+print(number1<=number2)
+print(number1!=number2)
+      
+print(number1> 10& number2>10)
+print(number1> 10 |number2>10)
+print(!(number1 >10))
+      
+
+number <- "100"
+str <- "R languge"
+result=number +str
+print(result)
+
+number <- "100"
+str <- "R languge"
+result=number +str
+print(result)
