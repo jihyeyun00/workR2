9c806f0110b3b5e08cb2b0dccb7de2d73c38e99e
jihyeyun00 <56308079+jihyeyun00@users.noreply.github.com>
2019-11-28 23:31:13 +0900

    homework

--- yun ji hye.R
+++ yun ji hye.R
@@ -1,50 +1,110 @@
-#ìœ¤ì§€í˜œ 11.28
-
-d1 <- 1:50;  d1   #1-1
+# À±ÁöÇý 11.28
+#1-1
+d1 <- 1:50;  d1   
 d2 <- 51:100; d2  
 
 
 x <- c(d1)
 y <- c(d2)
 
-x+y  #3
+
+#1-2
+length(y)
+#1-3
+x+y 
 x-y
 x*y
 y/x
 
-sum(x) #4
-sum(y)
-sum(x+y) #5
+#1-4
 
-max(y) #6
+sum(x) 
+sum(y)
+
+#1-5
+sum(x)+sum(y)
+
+#1-6
+max(y)
 min(y)
 
-mean(x) #7
+#1-7
+mean(x)
 mean(y)
-mean(y-x)
-sort(x,decreasing = TRUE) #8
-sort(x,decreasing = FALSE) #9
+abs(mean(x-y))
+
+#1-8
+sort(x,decreasing = TRUE) 
+#1-9
+sort(x,decreasing = FALSE) 
 sort(y,decreasing = FALSE)
 
-d3 <- union(x,y)#9
+#1-9?
+d3 <- head((sort(x,decreasing = FALSE),10) & head(sort(y,decreasing = FALSE),10))
 
+
+
+
+
+#2
 v1 <- 51:90 ; v1
-d <- v1
-d[v1<60]  #2-1
-length(d>70)  #2
-sum(d)        #3
-d[60<v1 & 73>v1]#4
-d[65>v1 | 80<v1]#5
 
-for (i in v1) {    #6
+v2<- v1
+
+#2-1
+seq(51,60) 
+
+#2-2
+length(v1<70)
+
+#2-3
+v2[sum(v1>65)]   
+
+#2-4
+v2[v1>60 & v1<73]
+
+#2-5
+v2[v1<65 & v1>80]
+
+#2-6
+for (i in v1) {    
   if(i %% 7==3)
     print(i)
 }
 
+#2-7?
+for (i in v1){   
+  if(i * 7)}
+v2[v1(i*7)] <- 0 
 
+#2-8?
+for(i in v1) {  
+  if(i %% 2==0){
+    sum(v1(i))
+  }
+}  
 
+#2-9?
+for (i in v1){
+  if(i %% 2==1)
+    | for (i > 80)
+      print(i)
+}
 
+#2-10?
+for (i in v1) {   
+  if (i %% 3==0)
+    if (i %% 5==0)
+      intersect(i,i)
+}
 
+#2-11?
+for (i in v1) {     
+  if(i %% 2==0)
+    i*2
+}
+
+#2-12?
 
 
 
