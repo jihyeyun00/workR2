d18ad4bd46c16d38e6b5e2ad60224bf856d3c4ba
workR2 <jhjhy0313@naver.com>
2019-12-09 18:26:45 +0900

    10일

---
+++ 10DAY.R
@@ -0,0 +1,248 @@
+#10일차
+##document -user's guide:사용자설명서 ,reference:명령 ,함수  
+#markdown 형식->보고서 작성용 ,(크기를 다양하게 활용할 수 있는)
+install.packages("tidyverse")
+library(ggplot2)
+
+#https://www.tidyverse.org/packages/
+#https://ggplot2.tidyverse.org/
+#http://rpubs.com/brandonkopp/creating-a-treemap-in-r
+
+library(tidyverse)
+
+dim(mpg)
+str(mpg)
+head(mpg)
+view(mpg)
+
+ggplot(data=mpg)+                           #ggplot, +, geom_point:산점도그리는 함수 /mapping할때aes 함수를 쓴것(x,y지정)
+  geom_point(mapping = aes(x=displ,y=hwy)) 
+
+
+month<-c(1,2,3,4,5,6)
+rain<-c(55,50,45,50,60,70)
+df<-data.frame(month,rain)
+df
+
+ggplot(df,aes(x=month,y=rain))+            #ggplot,+,mapping 생략도 가능
+  geom_bar(stat = "identity",              #stat :y, width: 폭, fill :막대색깔
+           width = 0.7,
+           fill="steelblue")
+
+
+
+ggplot(df,aes(x=month,y=rain))+            #ggplot,+,(mapping 생략도 가능)
+  geom_bar(stat = "identity",              #stat :y축값으로 막대높이를 정하라 , width: 폭, fill :막대색깔
+           width = 0.7,
+           fill="steelblue")+
+
+ggtitle("월별강수량")+
+theme(plot.title = element_text(size = 25,   #'월별강수량'의 꾸미기
+                                face = "bold",
+                                colour="steelblue"))+
+labs(x="월",y="강수량")+coord_flip()   #가로형막대로하라
+
+
+
+
+ggplot(iris,aes(x=Petal.Length))+        #범주형이라면 카운트 해야하지만 연속형은 카운트 할 수 없음
+  geom_histogram(binwidth = 0.6)        #연속형binwidth : 빈도 계산,꽃잎의 길이를 0.5단위로 카운트 해라
+
+   
+fillPalette<-c("red","yellow","green")
+
+ggplot(iris,aes(x=Sepal.Width, fill=Species,  #fill에는 범주형을,species 는 범주형 문자형이지만 숫자로 자동 변환 1,2,3...
+                color=Species))+              
+  geom_histogram(binwidth = 0.5,position = "dodge")+  #dodge품종별로 각각그리기
+  theme(legend.position = "top") +  #top 범례의 위치
+  scale_fill_manual(values=fillPalette)
+
+
+#ggplot:layer 방식,(계층방식:graphic 처리할때 쓰는) 각 함수를 호츨할때마다 바닥에 ggplot , 두번째 판 geom, histogram,  그 위에theme
+
+
+
+
+#ggplot2 Scatter chart
+
+ggplot(data=iris,mapping=aes(x=Petal.Length,       #ggpolt쓸때 data이름이랑, aes (x,y)값은 정해줘야 최소
+                             y=Petal.Width))+      #각각의 판마다 역할을 나눠줌
+  geom_point()    
+
+ggplot(data=iris)+
+ geom_point(mapping=aes(x=Petal.Length,
+                         y=Petal.Width)) 
+
+
+
+
+ggplot(data=iris,mapping=aes(x=Petal.Length,       
+                             y=Petal.Width,
+                             color=Species,
+                             shape=Species))+      
+  geom_point(size=3)+
+  ggtitle("꽃잎의 길이와 폭")+
+  theme(plot.title = element_text(size=25,
+                                  face="bold",
+                                  colour = 'red'))
+
+#컴퓨터 색상 지정 방식:RGB, ARGB
+#red, green, blue/ alpha + rgb
+#rgb->16진수 :#000000~#FFFFFF
+#16진수 한자리 =2진수(4자리,4bit)=nibble ->2bite 사용
+#argb=#00000000~#FFFFFFFF 
+
+
+
+#ggplot box plot
+ggplot(data = iris,mapping = aes(y=Petal.Length))+   #박스플롯은 y 값을 정해주면 됌 x값은 (폭)은 의미가 없음
+  geom_boxplot(fill="yellow")
+
+ggplot(data = iris,mapping = aes(y=Petal.Length,
+                                 fill=Species))+  
+  geom_boxplot()
+
+
+#ggplot Line chart 시계열 데이터를 쓸때 선그래프
+year<-1937:1960
+cnt<-as.vector(airmiles)
+df<-data.frame(year,cnt)
+head(df)
+
+ggplot(df,aes(x=year,y=cnt))+
+  geom_line(col="red")
+
+
+#ggplot 작성 graph 꾸미기(공통)
+str(economics)
+
+#사선
+ggplot(economics,aes(x=date,y=psavert))+
+  geom_line()+
+  geom_abline(intercept = 12.18671, #회귀선
+              slope = -0.0005444)
+#intercept :y 절편값 (회귀식에서 나온값, 함수 나옴)
+#slope : 기울기
+
+  
+#평행선
+ggplot(economics,aes(x=date,y=psavert))+
+  geom_line()+      
+  geom_hline(yintercept = mean(economics$psavert)) #평균값이상과 이하로 넣은것
+
+
+#수직선:특정한 날을 표현하고 싶을때
+x_inter<-filter(economics,
+                psavert==min(economics$psavert))$date
+ggplot(economics,aes(x=date,y=psavert))+
+  geom_line()+
+  geom_vline(xintercept = x_inter)
+
+
+#텍스트 추가
+ggplot(airquality,aes(x=Day, y=Temp))+   
+  geom_point()+
+  geom_text(aes(label=Temp,    #0을 주면 점 바로 오른쪽 위
+                vjust=-1,      #+1 하단 좌측 모서리
+                hjust=-1))     #-1 위 오른쪽
+
+#영역지정 및 화살표 표시
+ggplot(mtcars,aes(x=wt,y=mpg))+
+  geom_point()+
+  annotate("rect",      #사각형모양으로 강조 하겠다
+           xmin = 3,
+           xmax = 4,
+           ymin = 12,
+           ymax = 21,
+           alpha= 0.5, #알파 값은 투명도 :1에 가까울수록 불투명
+           fill="skyblue")+
+  annotate("segment",x=2.5, xend=3.7,   #segment는 화살표 , xend =길이
+           y=10, yend = 17, color="red",
+           arrow=arrow())+
+  annotate("text",x=2.5, y=10,          #x,y 는 글자가 시작되는 위치
+           label="point")
+
+
+
+#treemap
+install.packages("treemap")
+library(treemap)
+  
+
+data(GNI2014)
+dim(GNI2014)
+str(GNI2014)
+head(GNI2014)
+View(GNI2014)
+
+treemap(GNI2014,
+        index = c("continent","iso3"),   #계층구조 콤마로 여러개해도 돼?
+        vSize = "population",            #타일크기
+        vColor="GNI",                     #타일컬러
+        type="value",                   #타일컬러링 방법(값이 높은것은 진하게)
+        bg.labels="yellow",             #레이블배경색
+        title="World's GNI")            #제목
+
+
+st<-data.frame(state.x77)
+st<-data.frame(st,stname=rownames(st)) #data.frame(컬럼1=자료, 컬럼2=자료...)
+treemap(st,
+        index=c("stname"),
+        vSize = "Area",
+        vColor = "Income",
+        type="value",
+        title="미국주별수입")
+
+
+#산점도에 Bubble 추가 (bubble chart) 1)버블차트
+symbols(st$Illiteracy, st$Murder,    #원의 x, y 좌표
+        circles = st$Population,     #원의 반지를
+        inches = 0.3,               #원 크기 조절값
+        fg="white",                     #원 테두리 색
+        bg="lightgray",            #원 바탕색
+        lwd=1.5,                    #원 테두리선 두께
+        xlab='rate of Illiteracy',
+        ylab = "crime(murder) rate",
+        main= "Illiteracy and Crime")
+text(st$Illiteracy, st$Murder,      #텍스트 출력 x,y 좌표
+     rownames(st),                  #출력할 text
+     cex=0.6,                        #폰트크기
+     col ="brown")                    #폰트컬러
+
+
+#ggplot 이용 2)버블차트 The R Graph Gallery :http://r-graph-gallery.com/index.html
+# Libraries
+library(ggplot2)
+library(dplyr)
+library(plotly)
+library(viridis)
+library(hrbrthemes)
+
+# The dataset is provided in the gapminder library
+library(gapminder)
+data <- gapminder %>% filter(year=="2007") %>% dplyr::select(-year)
+
+# Interactive version
+p <- data %>%
+  mutate(gdpPercap=round(gdpPercap,0)) %>%
+  mutate(pop=round(pop/1000000,2)) %>%
+  mutate(lifeExp=round(lifeExp,1)) %>%
+  
+  # Reorder countries to having big bubbles on top
+  arrange(desc(pop)) %>%
+  mutate(country = factor(country, country)) %>%
+  
+  # prepare text for tooltip
+  mutate(text = paste("Country: ", country, "\nPopulation (M): ", pop, "\nLife Expectancy: ", lifeExp, "\nGdp per capita: ", gdpPercap, sep="")) %>%
+  
+  # Classic ggplot
+  ggplot( aes(x=gdpPercap, y=lifeExp, size = pop, color = continent, text=text)) +
+  geom_point(alpha=0.7) +
+  scale_size(range = c(1.4, 19), name="Population (M)") +
+  scale_color_viridis(discrete=TRUE, guide=FALSE) +
+  theme_ipsum() +
+  theme(legend.position="none")
+
+# turn ggplot interactive with plotly
+pp <- ggplotly(p, tooltip="text")
+pp
---
+++ yunjihye12.09.R
@@ -0,0 +1,140 @@
+# * 실습 결과를 R Script file로 제출
+# * R Script file 이름은 "영문본인이름_제출일날짜.R" 부여하여 제출
+# * R Script file의 처음에 주석으로 본인 이름과 작성일/제출일 기록
+# 
+# 문1)
+# R에서 제공하는 mtcars 데이터셋에서 gear(기어의 수)에 대해 ggplot으로
+# 막대그래프를 작성하시오. 단, 제목과 x축 레이블은 ‘기어의 수’, y축 레이블
+# 은 ‘빈도수’로 나타내시오.
+#
+str(mtcars)
+ggplot(mtcars,aes(x=gear,y="빈도수"))+            #ggplot,+,mapping 생략도 가능
+geom_bar(stat = "identity",              #stat :y, width: 폭, fill :막대색깔
+         width = 0.7,
+         fill="steelblue")+
+ggtitle("기어수와 빈도")+
+  theme(plot.title = element_text(size = 25,   
+                                  face = "bold",
+                                  colour="steelblue"))+
+  labs(x="기어의 수",y="빈도수")
+
+# 문2)
+# R에서 제공하는 mtcars 데이터셋에서 cyl(실린더의 수)에 대해 막대 색이
+# 초록색인 막대그래프를 ggplot으로 작성하시오.
+ggplot(mtcars,aes(x=cyl))+            #ggplot,+,mapping 생략도 가능
+  geom_bar(width = 0.7,
+           fill="green")+
+  ggtitle("실린더")+
+  theme(plot.title = element_text(size = 25,   
+                                  face = "bold",
+                                  colour="steelblue"))
+  labs(x="실린더의 수")
+# 
+# 문3) 
+# R에서 제공하는 mtcars 데이터셋에서 mpg(연비)에 대해 구간 간격이 5.0
+# 인 히스토그램을 ggplot으로 작성하시오. 
+# 
+ggplot(mtcars,aes(x=mpg))+
+  
+  geom_histogram(binwidth = 0.5)        #답 잘보자
+
+
+# 문4)??
+# R에서 제공하는 trees 데이터셋의 Girth(나무 둘레)에 대해 ggplot으로
+# 히스토그램을 작성하시오. 여기에서는 히스토그램의 제목, x축 레이블, y축
+# 레이블을 한글로 표시하시오. (구간 간격은 3.0, 막대의 색은 steelblue로 한다.)
+str(trees)
+head(trees)
+ggplot(trees,aes(x=as.factor(Girth))+       
+  geom_histogram(binwidth = 3.0)+        
+  ggtitle("나무둘레")+
+  theme(plot.title = element_text(size=25,
+                                  face="bold",
+                                  colour = "s"))  
+  
+# 문5)
+# R에서 제공하는 mtcars 데이터셋에서 mpg(연비)를 x축으로 하고, wt(중
+#                                             량)를 y축으로 하는 산점도를 ggplot으로 작성하시오. (단, 점의 색은 gear의
+#                                                                                수에 따라 다르게 표시한다.)
+
+  ggplot(data=mtcars,mapping=aes(x=mpg,       
+                              y=wt))+     
+  geom_point()      
+  
+# 문6)
+# R에서 제공하는 mtcars 데이터셋에서 mpg(연비)에 대해 ggplot으로 상
+# 자그림을 작성하되, cyl(실린더 수)에 따라 그룹을 나누어 작성하시오.
+  ggplot(data = mtcars,mapping = aes(x=as.factor(cyl),y=mpg))+   
+    geom_boxplot(fill="green")
+  
+ 
+# 문7) 
+# 다음은 2015년부터 2026년도까지의 예상 인구수 추계 자료이다. 연도를
+# x축으로 하여 ggplot으로 선그래프를 작성하시오.
+# 
+# 연도		총인구 (천명)		연도		총인구 (천명)
+# 2015		51014				2021		52123
+# 2016		51245				2022		52261
+# 2017		51446				2023		52388
+# 2018		51635				2024		52504
+# 2019		51811				2025		52609
+# 2020		51973				2026		52704	
+# 
+year<-2015:2026
+popu<-c(51014,51245,51446,51635,51811,51973,52123,52261,52388,52504,52609,52704)
+
+yp<-data.frame(year,popu)
+head(yp)
+ggplot(yp,aes(x=year,y=popu))+
+  geom_line(col="red")
+
+# 문8)
+# 다음과 같이 데이터셋 us를 생성한 후 물음에 답하시오. 여기서 state.x77
+# 은 미국 50개 주의 통계정보가, state.division은 미국 50개 주의 지역 구분
+# (예: 북부, 중부, 남부……) 정보가 저장된 데이터셋이다.
+# 
+# us <- data.frame(state.x77, state.division)
+st77 <- data.frame(state.x77)
+stdi<-data.frame(state.division)
+# (1) 미국 50개 주에 대해 각각의 주들이 지역구분별로 묶인 트리맵을 작성하시오.
+# 또한, 타일의 면적은 Population(인구수), 타일의 색은 Income(소득)으로 나타내고,
+# 각각의 타일에는 주의 이름을 표시하시오. 마지막으로 이 트리맵에서 관찰할 수 있
+# 는 것이 무엇인지 설명하시오
+
+
+data(us)
+head(us)
+str(us)
+View(us)
+treemap(us,
+        index = c("state.division",""),   #계층구조 콤마로 여러개해도 돼?
+        vSize = "population",            #타일크기
+        vColor="Income",                     #타일컬러
+        type="value",                   #타일컬러링 방법(값이 높은것은 진하게)
+        bg.labels="yellow",             #레이블배경색
+        title="us")           
+
+st<-data.frame(state.x77)
+st2<-data.frame(st,stname=rownames(stdi)) #data.frame(컬럼1=자료, 컬럼2=자료...)
+us<-data.frame(st,st2)
+treemap(us,
+        index=c("state.division",),
+        vSize = "Area",
+        vColor = "Income",
+        type="value",
+        title="미국주별수입")
+
+
+# (2) 미국 50개 주에 대해 각각의 주들이 지역구분별로 묶인 트리맵을 작성하시오.
+# 또한, 타일의 면적은 HS.Grad(고등학교 졸업률), 타일의 색은 Murder(범죄률)로 나타
+# 내고, 각각의 타일에는 주의 이름을 표시하시오. 마지막으로 이 트리맵에서 관찰할
+# 수 있는 것이 무엇인지 설명하시오.
+# 
+# (3) us 데이터셋에 대해 x축은 Income(소득), y축은 Illiteracy(문맹률), 원의 크기는
+# Population(인구수), 원의 색은 green(초록색), 원 내부에는 주의 이름을 표시한 버
+# 블차트를 작성하시오. 또한 이 버블차트에서 관찰할 수 있는 것이 무엇인지 설명하
+# 시오.
+# 
+# (4) us 데이터셋에 대해 x축은 Illiteracy(문맹률), y축은 Murder(범죄률), 원의 크기
+# 는 Area(면적), 원의 색은 green(초록색), 원 내부에는 주의 이름을 표시한 버블차트
+# 를 작성하시오. 또한 이 버블차트에서 관찰할 수 있는 것이 무엇인지 설명하시오.
